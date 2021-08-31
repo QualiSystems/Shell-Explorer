8e64e9124b66a69a2fc4c9f3d90459c6f9b24df6
Kyrylo Maksymenko <saklar13@gmail.com>
2021-08-31 17:21:26 +0300

    move test entities to tests

--- scripts/shell_explorer/entities.py
+++ scripts/shell_explorer/entities.py
@@ -124,31 +124,3 @@ class Shell2G(Repo):
 
 class Package(Repo):
     yaml_tag = u"!Package"
-
-
-if __name__ == '__main__':
-    shell1 = Shell2G('Test Shell2', 'http://sfsdfsfsdf2')
-    shell1.releases.append(Release('Test Rel1', '1.2.3', datetime.datetime.now(), 'http://fsafasf', 'PY2'))
-    shell1.releases.append(Release('Test Rel2', '1.2.4', datetime.datetime.now(), 'http://fsafasfff', 'PY3'))
-    #
-    shell2 = Shell2G('Test Shell1', 'http://sfsdfsfsdf1')
-    shell2.releases.append(Release('Test Rel1', '1.2.3', datetime.datetime.now(), 'http://fsafasf', 'PY2'))
-    shell2.releases.append(Release('Test Rel2', '1.2.4', datetime.datetime.now(), 'http://fsafasfff', 'PY3'))
-
-    shell3 = Shell1G('Test Shell2', 'http://sfsdfsfsdf2')
-    shell3.releases.append(Release('Test Rel1', '1.2.3', datetime.datetime.now(), 'http://fsafasf', 'PY2'))
-    shell3.releases.append(Release('Test Rel2', '1.2.4', datetime.datetime.now(), 'http://fsafasfff', 'PY3'))
-
-    shell4 = ShellL1('Test Shell4', 'http://sfsdfsfsdf2')
-    shell4.releases.append(Release('Test Rel1', '1.2.3', datetime.datetime.now(), 'http://fsafasf', 'PY2'))
-    shell4.releases.append(Release('Test Rel2', '1.2.4', datetime.datetime.now(), 'http://fsafasfff', 'PY3'))
-
-    shells = [shell1, shell2, shell3, shell4]
-    shells = sorted(shells, key=lambda a: a.yaml_tag)
-
-    dump = yaml.dump(shells, default_flow_style=False, sort_keys=False)
-
-    print(dump)
-
-    loaded = yaml.load(dump, Loader=yaml.Loader)
-    print(loaded)
